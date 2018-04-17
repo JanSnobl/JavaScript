a64119b38ce01677385c689e8bbf953aea2bd29c
JanSnobl <jsnobl89@gmail.com>
2018-04-17 10:44:09 +0200

    Added applications: GitHub Finder, Chuck Norris joke generator, Loan Calculator, Number Guesser, Task List.

---
+++ GitHubFinder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '3da055b4190b6adb6701';
+    this.client_secret = '807a008ef319bed701bcad401566fcf80a557935';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
